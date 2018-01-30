81243f2ffadd82cc632d6a5c4e1f2fc799ae7bce
Danny Chang <dannychachang@gmail.com>
2018-01-30 16:40:24 -0500

    Rewrite tests for reward and projects using FactoryGirl

--- test/models/pledge_test.rb
+++ test/models/pledge_test.rb
@@ -13,42 +13,11 @@ class PledgeTest < ActiveSupport::TestCase
   end
 
   test 'owner cannot back own project' do
-    # owner = build(:user)
-    # owner.save
     owner = create(:user)
     project = create(:project, user: owner)
     pledge = build(:pledge, user: owner, project: project)
 
-
-    # owner = new_user
-    # owner.save
-    # project = new_project
-    # project.user = owner
-    # project.save
-    # pledge = Pledge.new(dollar_amount: 3.00, project: project)
-    # pledge.user = owner
-    # pledge.save
     assert pledge.invalid?, 'Owner should not be able to pledge towards own project'
   end
-
-  # def new_project
-  #   Project.new(
-  #     title:       'Cool new boardgame',
-  #     description: 'Trade sheep',
-  #     start_date:  Date.today,
-  #     end_date:    Date.today + 1.month,
-  #     goal:        50000
-  #   )
-  # end
-  #
-  # def new_user
-  #   User.new(
-  #     first_name:            'Sally',
-  #     last_name:             'Lowenthal',
-  #     email:                 'sally@example.com',
-  #     password:              'passpass',
-  #     password_confirmation: 'passpass'
-  #   )
-  # end
 
 end
