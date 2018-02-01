7c2e6a89470353046c55539f2b3653591c21489f
Danny Chang <dannychachang@gmail.com>
2018-02-01 14:10:32 -0500

    Fix reward_check to read out when all rewards are maxed

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -15,14 +15,15 @@ class Pledge < ApplicationRecord
   end
 
   def reward_check
-    project.rewards.order(dollar_amount: :desc).each do |reward|
+    project.rewards.order(dollar_amount: :desc).map do |reward|
+      next if reward.reward_count == reward.reward_max
+        # add error to flash "All instances of #{reward.description} have been claimed!"
       if dollar_amount >= reward.dollar_amount && reward.reward_count != reward.reward_max
         reward.increment(:reward_count).save
-        return reward
-      else
-        false
+        return reward.description
       end
     end
+    return   "All of the rewards for this project have been claimed!"
   end
 
 
