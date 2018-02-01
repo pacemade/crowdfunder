4e7431744cf18e812f005e2761992be1671bfbeb
Danny Chang <dannychachang@gmail.com>
2018-02-01 10:33:08 -0500

    Re-add new project view partial form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131213759) do
+ActiveRecord::Schema.define(version: 20180201151916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20180131213759) do
     t.float "dollar_amount"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "reward_count", default: 0
+    t.integer "reward_max", default: 0
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
